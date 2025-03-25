5f26adf1be6ff35a840baa772aabe829f9e1abbd
ahto <ahto@artifact8.xyz>
2025-03-25 15:06:28 +1100

    fixing path of gridsynth in rottnestpy, builds and makes the gridsynth dependency

--- scripts/rottnestpy.sh
+++ scripts/rottnestpy.sh
@@ -2,18 +2,28 @@
 #
 # Used to setup rottnest py once it has been downloaded
 
-
+PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
 
 BUILDDIR=$1
 source $BUILDDIR/venv/bin/activate
 eval "$(pyenv init -)"
 pyenv global 3.11
 cd $BUILDDIR/rottnestpy
+
+make all
+
+RESRET=$?
+if test $RESRET -ne 0; then
+  echo 'Building rottnest_py failed: pip install'
+  exit 1
+else
+  exit 0
+fi
 pip install -e .
 
 RESRET=$?
 if test $RESRET -ne 0; then
-  echo 'Building rottnest_py failed'
+  echo 'Building rottnest_py failed: pip install'
   exit 1
 else
   exit 0
