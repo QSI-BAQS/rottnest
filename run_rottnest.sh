d88560a4a71f44de8ef704e8ab3ed19e38140660
Ben Y. <bjy777101@gmail.com>
2026-03-18 09:45:02 +1100

    Updated run_rottnest script to try to ensure it doesn't exit early

--- run_rottnest.sh
+++ run_rottnest.sh
@@ -26,9 +26,10 @@ echo -n "Starting rottnest."
 
 # Cursed trap kill-all on a subshell ensures ctrl-c kills everything...
 (trap 'kill 0' SIGINT; \
-python ./rottnest/applications/rottnest_py/src/rottnest/server/server.py & \
+python ./rottnest/applications/rottnest_py/src/rottnest/server/server.py >/dev/null 2>/dev/null & \
 echo -n '.'; sleep 2; echo -n '.'; \
 cd ./rottnest/applications/rottnest_js || (kill 0 && exit 2); \
 npx vite --port 5175 >/dev/null 2>/dev/null & \
-"$BROWSER" http://localhost:5175; kill 0)
-
+echo -n '.'; sleep 3; echo '.'; echo -e "\nUse <ctrl>+c to end the rottnest server"; $BROWSER http://localhost:5175; wait; kill 0)
+# ^ Unfortunately, we can't assume that $BROWSER will block, and so can't just exit when $BROWSER does
+# (eg. firefox only blocks if there isn't already an open instance)
