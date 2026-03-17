7c5659f9d03bd1429b69e05b0175fb7c7fb4f80e
Ben Y. <bjy777101@gmail.com>
2026-03-17 16:43:54 +1100

    Removed pandora from run_rottnest, added non-xdg browser selector

--- run_rottnest.sh
+++ run_rottnest.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# Create personalised pandora_pg.json
-echo "{ \"database\":\"postgres\", \"user\":\"$USER\", \"host\":\"localhost\", \"port\":\"5432\", \"password\":\"1234\" }" > ./pandora_pg.json
-
 if [[ ! -d "rottnest" ]]; then
-  echo "rottnest does not appear to be installed - have you run make?"
-  exit 1
+    echo "rottnest does not appear to be installed - have you run make?"
+    exit 1
 fi
 
 BROWSER="$(xdg-mime query default text/html)"
-BROWSER="${BROWSER/.desktop/}"
 
-echo -n "Starting rottnest"
+if [[ "$BROWSER" == "" ]]; then
+    if [[ $(which firefox; echo "$?") -eq 0 ]]; then
+        BROWSER=firefox
+    elif [[ $(which chrome; echo "$?") -eq 0 ]]; then
+        BROWSER=chrome
+    elif [[ $(which chromium; echo "$?") -eq 0 ]]; then
+        BROWSER=chromium
+    else
+        echo "Could not determine a web-browser to run the frontend with"
+        exit 1
+    fi
+else
+    BROWSER="${BROWSER/.desktop/}"
+fi
+
+echo -n "Starting rottnest."
 
 # Cursed trap kill-all on a subshell ensures ctrl-c kills everything...
-(trap 'kill 0' SIGINT; export TOP_DIR="$PWD"; \
-cd ./rottnest/applications/pandora || (kill 0 && exit 2); \
-./run_apptainer.sh >/dev/null 2>/dev/null & \
-# Sleeps are to provide startup time
-cd "$TOP_DIR" || (kill 0 && exit 2); echo -n '.'; sleep 5; echo -n '.'; \
-python ./rottnest/applications/rottnest_py/src/rottnest/server/server.py >/dev/null 2>/dev/null & \
-cd "$TOP_DIR" || (kill 0 && exit 2); echo -n '.'; sleep 2; echo -n '.'; \
+(trap 'kill 0' SIGINT; \
+python ./rottnest/applications/rottnest_py/src/rottnest/server/server.py & \
+echo -n '.'; sleep 2; echo -n '.'; \
 cd ./rottnest/applications/rottnest_js || (kill 0 && exit 2); \
 npx vite --port 5175 >/dev/null 2>/dev/null & \
 "$BROWSER" http://localhost:5175; kill 0)
